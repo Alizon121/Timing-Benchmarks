aedcf7256eede53bec64539d0973fc7017d7094f
Andrew <andrew.lizon12@gmail.com>
2024-05-07 13:26:17 -0700

    Completed phase 1

--- phase-1.js
+++ phase-1.js
@@ -1,16 +1,28 @@
 // Adds up positive integers from 1-n
+
+
+
+// How is this running in O(n) time?
+let startTime = Date.now()
 function addNums(n) {
-  // Fill this in
-
+let sum = 0;
+  for (let i = 1; i<= n; i++) {
+    sum += i
+  }
+return sum
 }
+let endTime = Date.now()
 
+const runTime = startTime - endTime
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
-
+let sum = 0;
+  for (let i = 1; i <= n; i++) {
+   sum += addNums(i)
+  }
+  return sum 
 }
-
 
 
 module.exports = [addNums, addManyNums];
